d23a317590879455e1267ceef5b859f19c25c4f6
NuntapongS <nuntapong@odds.team>
2023-06-22 17:16:32 +0700

    feat: count number page

--- src/pages/countNumber.tsx
+++ src/pages/countNumber.tsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 const CountNumber = () => {
-  return <div>Hello</div>;
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <div className="flex flex-col">
+        <div className="flex justify-center">
+          <button
+            className="flex justify-center items-center bg-black text-white rounded-lg h-11 w-36 my-6"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            increase
+          </button>
+        </div>
+        <div className="flex justify-center">count: {count}</div>
+        <div className="flex justify-center ">
+          <button
+            className="flex justify-center items-center bg-black text-white rounded-lg h-11 w-36 my-6 "
+            disabled={count <= 0}
+            onClick={() => setCount((count) => count - 1)}
+          >
+            decrease
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CountNumber;
