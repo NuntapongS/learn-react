ebfdc76b58dd334b3872956383ad0528b768122a
NuntapongS <nuntapong@odds.team>
2023-06-22 16:24:12 +0700

    feat: set default router page is count number

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import React from "react";
-import logo from "./logo.svg";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import "./App.css";
+import CountNumber from "./pages/countNumber";
+
+const route = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/count-number" />,
+  },
+  {
+    path: "/count-number",
+    element: <CountNumber />,
+  },
+]);
 
 function App() {
-  return (
-    <div className="flex justify-center text-lg font-bold ">Hello World</div>
-  );
+  return <RouterProvider router={route} />;
 }
 
 export default App;
