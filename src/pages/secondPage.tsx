e60d29b9135c21a64b1b7ba5614a77d3ca14527e
NuntapongS <nuntapong@odds.team>
2023-06-23 09:01:08 +0700

    feat: add back button in temp second page

--- src/pages/secondPage.tsx
+++ src/pages/secondPage.tsx
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import NavigateButton from "../components/NavigateButton";
+
 const SecondPage = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <div>This page is temp.</div>
+      <NavigateButton
+        message={"Back"}
+        OnClick={() => {
+          navigate("/count-number");
+        }}
+      />
     </>
   );
 };
