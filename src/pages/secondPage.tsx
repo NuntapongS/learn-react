2c2599ac4592820f3b391507cc9908752ca81f15
NuntapongS <nuntapong@odds.team>
2023-07-04 21:05:37 +0700

    add button to learn set state

--- src/pages/secondPage.tsx
+++ src/pages/secondPage.tsx
@@ -1,12 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import NavigateButton from "../components/NavigateButton";
+import { useState } from "react";
 
 const SecondPage = () => {
   const navigate = useNavigate();
+  const [name, setName] = useState("Click to set name");
+  const [text, setText] = useState("Click Me");
 
   return (
     <>
-      <div className="flex justify-center">This page is temp 2.</div>
+      <div className="flex justify-center">
+        <button
+          className="flex my-5 h-11 w-40 justify-center items-center bg-black text-white"
+          onClick={() => {
+            setName("Nuntapong");
+          }}
+        >
+          {name}
+        </button>
+      </div>
       <div className="flex justify-between mx-80 mt-16">
         <NavigateButton
           message={"Next"}
@@ -18,6 +30,17 @@ const SecondPage = () => {
           message={"Back"}
           OnClick={() => navigate("/count-number")}
         />
+      </div>
+
+      <div className="flex justify-center">
+        <button
+          className="flex my-5 h-11 w-40 justify-center items-center bg-black text-white"
+          onClick={() => {
+            setText("Thank you");
+          }}
+        >
+          {text}
+        </button>
       </div>
     </>
   );
