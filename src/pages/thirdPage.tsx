3284a461610f355be335c7883a181079b895d623
NuntapongS <nuntapong@odds.team>
2023-07-04 09:05:46 +0700

    add third page

---
+++ src/pages/thirdPage.tsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+import NavigateButton from "../components/NavigateButton";
+
+const ThirdPage = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="flex justify-center">This page is temp 3.</div>
+      <div className="flex justify-between mx-80 mt-16">
+        <NavigateButton
+          message={"Next"}
+          OnClick={() => {
+            navigate("");
+          }}
+          disabled={true}
+        />
+        <NavigateButton
+          message={"Back"}
+          OnClick={() => navigate("/second-page")}
+        />
+      </div>
+    </>
+  );
+};
+
+export default ThirdPage;
