350cb55de01ee474ec9ecb7a12ec2009ff863b8a
NuntapongS <nuntapong@odds.team>
2023-07-12 17:38:55 +0700

    refactor: when click name button

--- src/pages/secondPage.tsx
+++ src/pages/secondPage.tsx
@@ -2,19 +2,30 @@ import { useNavigate } from "react-router-dom";
 import NavigateButton from "../components/NavigateButton";
 import { useState } from "react";
 
+type buttonStyle = {
+  background: string;
+  textColor: string;
+};
+
 const SecondPage = () => {
   const navigate = useNavigate();
   const [name, setName] = useState("Click to set name");
   const [text, setText] = useState("Click Me");
-  const [background, setBackground] = useState("bg-black");
+  const [{ background, textColor }, setButtonStyle] = useState<buttonStyle>({
+    background: "bg-black",
+    textColor: "text-white",
+  });
   return (
     <>
       <div className="flex justify-center">
         <button
-          className={`flex my-5 h-11 w-40 justify-center items-center ${background} text-white`}
+          className={`flex my-5 h-11 w-40 justify-center items-center ${background} ${textColor}`}
           onClick={() => {
             setName("Nuntapong");
-            setBackground("bg-red-500");
+            setButtonStyle({
+              background: "bg-red-500",
+              textColor: "text-black",
+            });
           }}
         >
           {name}
